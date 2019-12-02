3223deb2ea84e52794e681f8667217053e8ca649
Macbook pro retina <devarthurribeiro@gmail.com>
2019-12-02 04:11:29 -0300

    add componente DynamicForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Form, Input, Select } from '@rocketseat/unform';
+import DynamicForm from "./DynamicForm";
 
 import form1 from "./forms/form1";
+
 import "./App.css";
- 
- 
+
 function handleSubmit(data) {
   console.log(data);
 }
@@ -13,26 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">GTI forms</h1>
-      <Form onSubmit={(handleSubmit)}>
-      { form1.map( input => {
-        if(input.type === 'select') {
-          return (
-            <div key={input.name}>
-              <label>{input.placeholder}</label>
-              <Select name={input.name} options={input.options} className="select" />
-            </div>
-          )
-        }
-        return (
-            <div key={input.name}>
-              <Input placeholder={input.placeholder} name={input.name} className="input" /> 
-            </div>
-          )
-      })}
-      <center>
-        <button className="button is-success" type="submit">Enviar</button>
-      </center>
-    </Form>
+      <DynamicForm form={form1} onSubmit={handleSubmit} />
     </div>
   );
 }
