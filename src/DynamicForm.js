b7a9d83b8331aee555cc4eb1b8a950ed109b039d
Macbook pro retina <devarthurribeiro@gmail.com>
2019-12-02 13:33:40 -0300

    Melhorias de ui

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -3,32 +3,31 @@ import { Form, Input, Select } from "@rocketseat/unform";
 
 function DynamicForm(props) {
   if (props.form === null) {
-    return (
-      null
-    )
+    return null;
   }
   return (
     <Form onSubmit={props.onSubmit}>
       {props.form.map(input => {
         if (input.type === "select") {
           return (
-            <div key={input.name}>
-              <label>{input.placeholder}</label>
-              <Select
-                name={input.name}
-                options={input.options}
-                className="select"
-              />
+            <div key={input.name} className="field">
+              <div className="control">
+                <div className="select is-fullwidth">
+                    <Select name={input.name} placeholder={input.placeholder} options={input.options} />
+                </div>
+              </div>
             </div>
           );
         }
         return (
-          <div key={input.name}>
+          <div key={input.name} className="field">
+            <div className="control">
             <Input
               placeholder={input.placeholder}
               name={input.name}
-              className="input"
+              className="input is-fullwidth"
             />
+            </div>
           </div>
         );
       })}
