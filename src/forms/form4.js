6804af9aadbfe563ccd37f123817b9892126bdce
Macbook pro retina <devarthurribeiro@gmail.com>
2019-12-02 08:26:37 -0300

    add form estabilizado/nobreak

---
+++ src/forms/form4.js
@@ -0,0 +1,48 @@
+const form4 = [
+  {
+    name: "state",
+    placeholder: "Tipo",
+    type: "select",
+    options: [
+      { id: "1", title: "ESTABILIZADOR" },
+      { id: "2", title: "NOBREAK" },
+    ]
+  },
+  {
+    name: "code",
+    placeholder: "Código und.",
+    type: "text"
+  },
+  {
+    name: "tombo",
+    placeholder: "Tombo",
+    type: "text"
+  },
+  {
+    name: "serial",
+    placeholder: "Serial",
+    type: "text"
+  },
+  {
+    name:'brand',
+    placeholder: 'Fabricante',
+    type: 'text'
+  },
+  {
+    name:'model',
+    placeholder: 'Modelo',
+    type: 'text'
+  },
+  {
+    name:'power',
+    placeholder: 'Potencia',
+    type: 'text'
+  },
+  { 
+    name:'pcName',
+    placeholder: 'Nome pc',
+    type: 'text'
+  },
+];
+
+export default form4;
