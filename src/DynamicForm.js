2b0470eefd230a1f37b36ed3a3a55d796c90239d
Macbook pro retina <devarthurribeiro@gmail.com>
2019-12-03 10:40:14 -0300

    uso de ternário na logica do input

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -5,34 +5,40 @@ function DynamicForm(props) {
   if (props.form === null) {
     return null;
   }
+  
   return (
     <Form onSubmit={props.onSubmit}>
-      {props.form.map(input => {
-        if (input.type === "select") {
-          return (
-            <div key={input.name} className="field">
-              <div className="control">
-                <div className="select is-fullwidth">
-                    <Select name={input.name} placeholder={input.placeholder} options={input.options} />
-                </div>
-              </div>
-            </div>
-          );
-        }
-        return (
+      {props.form.map(input =>
+        input.type === "select" ? (
           <div key={input.name} className="field">
             <div className="control">
-            <Input
-              placeholder={input.placeholder}
-              name={input.name}
-              className="input is-fullwidth"
-            />
+              <div className="select is-fullwidth">
+                <Select
+                  name={input.name}
+                  placeholder={input.placeholder}
+                  options={input.options}
+                />
+              </div>
             </div>
           </div>
-        );
-      })}
+        ) : (
+          <div key={input.name} className="field">
+            <div className="control">
+              <Input
+                placeholder={input.placeholder}
+                name={input.name}
+                className="input is-fullwidth"
+              />
+            </div>
+          </div>
+        )
+      )}
       <center>
-        <button disabled={props.isLoading} className="button is-success" type="submit">
+        <button
+          disabled={props.isLoading}
+          className="button is-success"
+          type="submit"
+        >
           Enviar
         </button>
       </center>
