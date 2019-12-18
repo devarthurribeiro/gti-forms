45252cec606127e56b3399766d5e9e1c78b592b4
Macbook pro retina <devarthurribeiro@gmail.com>
2019-12-18 14:06:48 -0300

    correcao  de rotas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
-import ListForms from "./components/ListForms";
+  import React, { useState } from "react";
+  import ListForms from "./components/ListForms";
 
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+  import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Router path="/">
-          <ListForms />
-        </Router>
-      </Switch>
-    </Router>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/forms">
+            <ListForms />
+          </Route>
+          <Route path="/">
+            <div className="section">
+              <h1 className="title">App</h1>
+              <div className="columns is-mobile">
+                <div className="column">
+                  <Link to="/">Home</Link>
+                </div>
+                <div className="column">
+                  <Link to="/forms">Forms</Link>
+                </div>
+                <div className="column">3</div>
+                <div className="column">4</div>
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
